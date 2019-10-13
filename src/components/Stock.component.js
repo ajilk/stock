befdfc640b7df64fd5719ce7687cb6a7d13bd98d
ajilk <ajilkhomov@gmail.com>
2019-10-13 15:25:18 -0400

    Display currPrice and style it accordingly

--- src/components/Stock.component.js
+++ src/components/Stock.component.js
@@ -1,40 +1,47 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import { API_KEY, API_URL } from '../../src/config/alphaVantageConfig'
 
-function Stock(props) {
-  const [hovered, setHovered] = useState(false)
-  const toggleHover = () => setHovered(!hovered)
-  const { stock } = props
-  var openPrice, currPrice;
-  // Get price (curr, open)
-  fetch(API_URL + 'GLOBAL_QUOTE&symbol=' + stock.name + '&apikey=' + API_KEY)
-    .then(result => result.json())
-    .then(
-      result => {
-        if(result['Global Quote'] === undefined) return 
-        openPrice = parseInt(result['Global Quote']['02. open'])
-        currPrice = parseInt(result['Global Quote']['05. price'])
-      },
-      error => console.log(error)
-    )
+class Stock extends Component {
+  state = {
+    hovered: false,
+    openPrice: '',
+    currPrice: '',
+  }
 
-  const status = currPrice < openPrice ? "m-0 text-danger" : "m-0 text-success"
+  toggleHover = () => this.setState({ hovered: !this.state.hovered })
 
-  return (
-    <div className={`row py-2 border ${hovered ? "border-dark" : ""}`}
-      onMouseEnter={toggleHover}
-      onMouseLeave={toggleHover}
-    >
-      <div className="col-4 text-left my-auto">
-        <h6 className="m-0"><b>symbol: </b> {stock.name}</h6>
+  render() {
+    const { stock } = this.props
+    // Get price (curr, open)
+    fetch(API_URL + 'GLOBAL_QUOTE&symbol=' + stock.name + '&apikey=' + API_KEY)
+      .then(result => result.json())
+      .then(
+        result => {
+          if (result['Global Quote'] === undefined) return
+          this.setState({
+            openPrice: parseFloat(result['Global Quote']['02. open']),
+            currPrice: parseFloat(result['Global Quote']['05. price'])
+          })
+        },
+        error => console.log(error)
+      )
+    const status = this.state.openPrice > this.state.currPrice ? 'm-0 text-danger' : 'm-0 text-success'
+    return (
+      <div className={`row py-2 border ${this.state.hovered ? "border-dark" : ""}`}
+        onMouseEnter={this.toggleHover}
+        onMouseLeave={this.toggleHover}
+      >
+        <div className="col-4 text-left my-auto">
+          <h6 className="m-0"><b>symbol: </b> {stock.name}</h6>
+        </div>
+        <div className="col-4 text-left my-auto">
+          <h6 className="m-0"><b>quanity: </b>{stock.quantity}</h6>
+        </div>
+        <div className="col-4 text-left my-auto">
+          <h6 className={status}>${this.state.currPrice}</h6>
+        </div>
       </div>
-      <div className="col-4 text-left my-auto">
-        <h6 className="m-0"><b>quanity: </b>{stock.quantity}</h6>
-      </div>
-      <div className="col-4 text-left my-auto">
-        <h6 className={status}>${currPrice}</h6>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 export default Stock
